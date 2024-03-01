3979b971011123cccf90d43ab97b1bcb4f7150e8
kpcastillo <krn_iveth09@hotmail.com>
2024-03-01 13:20:11 -0800

    edited test

--- test/karen.tets.js
+++ test/karen.tets.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 
-it("test Karen",async ()=>{
+it('test Karen',async ()=>{
 const response = await fetch('http://localhost:3000/karen');
 const name = await response.text();
 expect.(name).toBe("karen");
